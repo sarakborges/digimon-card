e7f965eb14149d32fb338634f503a9d2907a79a8
Sara Borges <sarakborges@outlook.com>
2026-09-15 18:46:06 -0300

    feat: open heroes screen from library

--- scripts/ui/library_screen.gd
+++ scripts/ui/library_screen.gd
@@ -1,12 +1,23 @@
 extends Control
 
 const START_SCENE := "res://scenes/start_screen.tscn"
+const HEROES_SCENE := "res://scenes/heroes_screen.tscn"
 
+@onready var side_menu = $SideMenu
 @onready var return_button: Button = $ReturnButton
 
 
 func _ready() -> void:
+	side_menu.item_pressed.connect(_on_side_menu_item_pressed)
 	return_button.pressed.connect(_on_return_pressed)
+
+
+func _on_side_menu_item_pressed(item_id: StringName) -> void:
+	match String(item_id):
+		"heroes":
+			get_tree().change_scene_to_file(HEROES_SCENE)
+		"cards":
+			pass
 
 
 func _on_return_pressed() -> void:
